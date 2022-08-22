2ff7d92a0d7d0eaa8ba7ba2af0d67fedec3ffed9
Ryan <ryancaimano@gmail.com>
2022-08-22 14:42:45 -0500

    basic structure is setup. Table renders on page, although it is only temporary because I intend on creating a separate component for importing the data.

---
+++ src/Home.js
@@ -0,0 +1,27 @@
+const Home = () => {
+  return (
+    <><div>
+      <h1>Contact List</h1>
+    </div><table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">Profile Picture</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td ></td>
+            <td ></td>
+            <td ></td>
+            <td ></td>
+          </tr>
+        </tbody> 
+      </table>
+    </>
+  );
+}
+
+export default Home;
