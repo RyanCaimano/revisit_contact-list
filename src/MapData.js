57e0f743a39643560f520092bc867a261c96c073
Ryan <ryancaimano@gmail.com>
2022-08-22 22:44:34 -0500

    Went back and reviewed passing state from 'router-fun' ...created ContactNew component, have yet to complete addContact functional component to app.js. Table is aligned, fit to appropriate size, and text and images are centered. Next up is finish addContact and setting state for the addition of new contact.

--- src/MapData.js
+++ src/MapData.js
@@ -8,13 +8,23 @@ const MapData = (contact) => {
     <div>
       <h1>Contact List</h1>
       <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Profile Picture</th>
+            <th>Name</th>
+            <th>Email address</th>
+            <th>Phone Number</th>
+          </tr>
+        </thead>
         {data.contacts.map(contact => (
-          <tbody>
-            <tr>
-            <td><img src={`${contact.image_url}`} alt="here is your friend's portrait"/></td>
-              <td>{`${contact.name}`}</td>
-              <td>{`${contact.email}`}</td>
-              <td>{`${contact.phone_number}`}</td>
+          <tbody key={contact.alias}>
+            <tr key={contact.id}>
+              <td key={`${contact.image_url}`}>
+                <img src={`${contact.image_url}`} alt="here is your friend's portrait"/>
+              </td>
+              <td className="column2" key={`${contact.name}`}>{`${contact.name}`}</td>
+              <td className="column3" key={`${contact.email}`}>{`${contact.email}`}</td>
+              <td className="column4" key={`${contact.phone_number}`}>{`${contact.phone_number}`}</td>
             </tr> 
           </tbody>
         ))};
--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ const data =
     "contacts": [
     {
       "id": 42226184,
+      "alias": "Figgy",
       "name": "Isaac Newton",
       "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Portrait_of_Sir_Isaac_Newton%2C_1689.jpg/498px-Portrait_of_Sir_Isaac_Newton%2C_1689.jpg",
       "email": "lincolnshire@example.com",
@@ -11,6 +12,7 @@ const data =
 
     {
       "id": 96436090,
+      "alias": "Geometer",
       "name": "Euclid of Megara",
       "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Jusepe_de_Ribera_-_Euclid_-_2001.26_-_J._Paul_Getty_Museum.jpg/442px-Jusepe_de_Ribera_-_Euclid_-_2001.26_-_J._Paul_Getty_Museum.jpg",
       "email": "elements@example.com",
@@ -19,9 +21,10 @@ const data =
 
     {
       "id": 85907747,
+      "alias": "Phineas",
       "name": "Richard Feynman",
       "image_url": "https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Richard_Feynman_Nobel.jpg/220px-Richard_Feynman_Nobel.jpg",
-      "email": "celestial@example.com",
+      "email": "feynman_method@example.com",
       "phone_number": "15555555555"
         
     }
