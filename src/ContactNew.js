85d7d015f96b8793d795fac033b6e35868769919
Ryan <ryancaimano@gmail.com>
2022-08-25 13:48:58 -0500

    PersonalPage does not render. State is not properly set. Unsure where to direct the new contact form data. Hardly any progress over ten hours of work, so moving on for now.

--- src/ContactNew.js
+++ src/ContactNew.js
@@ -1,29 +1,58 @@
-// import {useState} from 'react'
+import {useState} from 'react'
+import {Link}  from 'react-router-dom'
 
 
-// const ContactNew = (props) => {
-//   const [image_url, setImage_url] = useState('')
-//   const [name, setName] = useState('')
-//   const [email, setEmail] = useState('')
-//   const [phone_number, setPhone_number] = useState('')
+const ContactNew = (props) => {
+  const [image_url, setImage_url] = useState('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone_number, setPhone_number] = useState('')
 
-//   const handleSubmit = () => {
-//     props.addContact({
-//       image_url,
-//       name,
-//       email,
-//       phone_number
-//     })
+  const handleSubmit = () => {
+    props.addContact({
+      image_url,
+      name,
+      email,
+      phone_number
+    })
 
-//     props.history.push('/')
+    props.history.push('/')
 
-//   }
+  }
 
-//   return (
-//     <div>
+  return (
+    <div>
+      <form>
+        <label>Image Url</label>
+        <input type='text' className='form-control'onChange={event =>
+          setImage_url(event.target.value)
+        }/>
 
+        <br/>
 
+        <label>Name</label>
+        <input type='text' className='form-control'onChange={event =>
+          setName(parseInt(event.target.value))
+        }/>
 
-// }
+        <br/>
 
-// export default ContactNew;
+        <label>Email Address</label>
+        <input type='text' className='form-control'onChange={event =>
+          setEmail(event.target.value)
+        }/>
+
+        <label>Phone Number</label>
+        <input type='text' className='form-control'onChange={event =>
+          setPhone_number(event.target.value)
+        }/>
+
+        <button type="button" onClick={handleSubmit}>Submit</button>
+      </form>
+
+      <Link to='/'>Back</Link>
+    </div>
+  )
+}
+
+export default ContactNew;
